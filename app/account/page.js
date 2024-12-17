21fda34d53b45a9ced474c7103f0732d8bad0df0
Ziad Tamer <ziadtamer0541@gmail.com>
2024-12-17 18:34:54 +0200

    v4

--- app/account/page.js
+++ app/account/page.js
@@ -3,6 +3,7 @@ import SignOutButton from "../_components/SignOutButton";
 
 export default async function Page() {
   const session = await auth();
+  console.log(session);
   if (!session) {
     return <p>You are not signed in.</p>;
   }
@@ -10,6 +11,7 @@ export default async function Page() {
   return (
     <div className="px-4 py-6 flex flex-col gap-6">
       <p>Welcome, {session.user.name}!</p>
+      <p>{session.user.email}</p>
       <SignOutButton />
     </div>
   );
